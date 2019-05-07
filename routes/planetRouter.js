51769021a09d32ec060e9c38f92cec2ab488ef00
Sidgi Samanduyev <samanduyev@gmail.com>
2019-05-06 20:27:07 -0400

    added and teste route planets. Working!

--- routes/planetRouter.js
+++ routes/planetRouter.js
@@ -2,9 +2,14 @@ const express = require('express');
 const {Planet} = require('../models')
 const planetRouter = express.Router();
 
-planetRouter.get('/planets',async(req,res)=>{
-  const allPlanets = await Planet.findAll();
-  res.json(allPlanets);
+planetRouter.get('/',async(req,res)=>{
+  try{
+    const allPlanets = await Planet.findAll();
+    res.send(allPlanets);
+  }catch(e){
+    console.log(e.message)
+  }
+  
 });
 
 module.exports = planetRouter;
--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const bodyParser = require('body-parser');
 const logger = require('morgan');
 const app = express();
 const PORT = 3000;
-app.get('/',async(req,res)=>{
-  res.send('Test message')
-});
+const planetRouter = require('./routes/planetRouter')
+app.use('/planets', planetRouter)
+
 app.listen(PORT,()=>{ console.log('listening port '+ PORT) })
